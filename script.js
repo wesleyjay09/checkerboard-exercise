d2791089fd050366fdd968fc2dbe5bd2e3359996
Wesley Coleman <77476122+wesleyjay09@users.noreply.github.com>
2021-02-03 17:00:45 -0600

    checkerBoard

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-// Your JS goes here
+createCheckers()
+
+function createDiv() {
+     return document.createElement('div')    
+}
+
+function createCheckers() {
+    var alt;
+    var body = document.querySelector('body')
+    for(var i = 0; i < 81; i++) {
+        var div = createDiv()
+        div.style.width = '11.1%'
+        div.style.float = 'left'
+        div.style.paddingBottom = '11.1%'
+        if(alt) {
+            div.style.background = 'black'
+            alt = false
+         }else{
+            div.style.background = 'red'
+            alt = true;
+        }
+        body.appendChild(div)
+        }
+    }
+    
+    function createCheckers() {
+        var alt;
+        var body = document.querySelector('body')
+        for(var i = 0; i < 81; i++) {
+            var div = createDiv()
+            div.style.width = '11.1%'
+            div.style.float = 'left'
+            div.style.paddingBottom = '11.1%'
+            if(alt) {
+                div.style.background = 'black'
+                alt = false
+             }else{
+                div.style.background = 'red'
+                alt = true;
+            }
+            body.appendChild(div)
+            }
+        }
